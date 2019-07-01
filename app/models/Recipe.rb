0caa5b251d8e1645e03bf159870d2c4ba71dc92b
Dave Bushnell <dybushnell@gmail.com>
2019-07-01 14:09:14 -0400

    v5

--- app/models/Recipe.rb
+++ app/models/Recipe.rb
@@ -1,46 +1,70 @@
-require 'pry'
+require "pry"
+
 class Recipe
-    attr_accessor :name
-    attr_reader
-    attr_writer
-    @@all = []
-    
-    def initialize(name)
-        @name = name
-        @@all.push(self)
-    end
+  attr_accessor :name
+  attr_reader
+  attr_writer
+  @@all = []
 
-    # Recipe.all should return all of the recipe instances
-    def self.all
-        @@all
-    end
-    
-    #should return the recipe instance with the highest number of users (the recipe that has the most recipe cards)
-    def self.most_popular
-    end
+  def initialize(name)
+    @name = name
+    @@all.push(self)
+  end
 
-    #should return the user instances who have recipe cards with this recipe
-    def users
+  # Recipe.all should return all of the recipe instances
+  def self.all
+    @@all
+  end
 
+  #should return the recipe instance with the highest number of users (the recipe that has the most recipe cards)
+  def self.most_popular
+  end
 
+  #should return the user instances who have recipe cards with this recipe
+  def users
+    userlist = []
 
-# userlist = []
-
-#         RecipeCard.all.select do |recipe_card|
-#             recipe_card.name == self
-#             userlist << recipe_card
-#         end
+    RecipeCard.all.select do |recipe_card|
+      recipe_card.recipe == self
+      userlist << recipe_card.user
     end
+    userlist
+  end
 
-    #should return all of the ingredients in this recipe
-    def ingredients
+  #should return all of the ingredients in this recipe
+  def ingredients
+    ingredient_list = []
+    RecipeIngredient.all.select do |recipe_ingredient|
+      if recipe_ingredient.recipe == self
+        ingredient_list << recipe_ingredient.ingredient #array containing all ingredients of this recipe
+      end
     end
-    
-    #hould return all of the Ingredients in this recipe that are allergens for Users in our system.
-    def allergens
+    ingredient_list
+  end
+
+  #should return all of the Ingredients in this recipe that are allergens for Users in our system.
+  def allergens
+    temp_list = []
+    allergenlist = []
+    RecipeCard.all.select do |recipe_card|
+      if recipe_card.recipe == self
+        temp_list << recipe_card.user #array containing all users of this recipe
+      end
+      i = 0
+      while i < temp_list.length
+        Allergy.all.select do |allergy_instance|
+          temp_list[i] == allergy_instance.user
+          allergenlist << allergy_instance.ingredient
+          i += 1
+        end
+      end
     end
-    
-    #should take an array of ingredient instances as an argument, and associate each of those ingredients with this recipe
-    def add_ingredients
-    end
+    #return temp_list
+
+    allergenlist
+  end
+
+  #should take an array of ingredient instances as an argument, and associate each of those ingredients with this recipe
+  def add_ingredients
+  end
 end
--- app/models/RecipeIngredient.rb
+++ app/models/RecipeIngredient.rb
@@ -1,28 +1,29 @@
-require 'pry'
+require "pry"
+
 class RecipeIngredient
-    attr_accessor :recipe, :ingredient
-    attr_reader
-    attr_writer
-    @@all = []
-    
-    def initialize(recipe, ingredient)
-        @recipe = recipe
-        @ingredient = ingredient
-        @@all.push(self)
-    end
-    
-    #should return all of the RecipeIngredient instances
-    def self.all
-        @@all
-    end
-    
-    #should return the ingredient instance
-    def ingredient
-        self.ingredient
-    end
-    
-    #should return the recipe instance
-    def recipe
-        self.recipe
-    end
+  attr_accessor :recipe, :ingredient
+  attr_reader
+  attr_writer
+  @@all = []
+
+  def initialize(recipe, ingredient)
+    @recipe = recipe
+    @ingredient = ingredient
+    @@all.push(self)
+  end
+
+  #should return all of the RecipeIngredient instances
+  def self.all
+    @@all
+  end
+
+  # #should return the ingredient instance
+  # def ingredient
+  #     self.ingredient
+  # end
+
+  # #should return the recipe instance
+  # def recipe
+  #     self.recipe
+  # end
 end
