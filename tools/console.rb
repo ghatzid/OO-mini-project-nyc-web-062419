8f1253dc3256542e5e1d3763706d24da71e54677
Dave Bushnell <dybushnell@gmail.com>
2019-07-01 14:12:48 -0400

    v6

--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,5 @@
-require 'pry'
-require_relative '../config/environment.rb'
+require "pry"
+require_relative "../config/environment.rb"
 
 # User(name)
 user1 = User.new("Bob")
@@ -10,6 +10,8 @@ user3 = User.new("Lisa")
 ingredient1 = Ingredient.new("potatoes")
 ingredient2 = Ingredient.new("apples")
 ingredient3 = Ingredient.new("tomatoes")
+ingredient4 = Ingredient.new("pollen")
+ingredient5 = Ingredient.new("ragweed")
 
 #Recipe(name)
 recipe1 = Recipe.new("Potato Pie")
@@ -20,33 +22,23 @@ recipe3 = Recipe.new("Tomato Pie")
 recipeingredient1 = RecipeIngredient.new(recipe1, ingredient1)
 recipeingredient2 = RecipeIngredient.new(recipe2, ingredient2)
 recipeingredient3 = RecipeIngredient.new(recipe3, ingredient3)
+recipeingredient4 = RecipeIngredient.new(recipe1, ingredient4)
+recipeingredient5 = RecipeIngredient.new(recipe2, ingredient5)
 
 # RecipeCard(recipe, user, date)
-recipecard1  = RecipeCard.new(recipe1,user1,9891)
-recipecard2  = RecipeCard.new(recipe2,user2,8302)
-recipecard3  = RecipeCard.new(recipe3,user3,3242)
-recipecard4  = RecipeCard.new(recipe3,user1,3456)
+recipecard1 = RecipeCard.new(recipe1, user1, 9891)
+recipecard2 = RecipeCard.new(recipe2, user2, 8302)
+recipecard3 = RecipeCard.new(recipe3, user3, 3242)
+recipecard4 = RecipeCard.new(recipe3, user1, 3456)
 
 # Allergy(user, ingredient)
-allergy1 = Allergy.new(user1,ingredient1)
-allergy2 = Allergy.new(user2,ingredient2)
-allergy3 = Allergy.new(user3,ingredient3)
-
-
+allergy1 = Allergy.new(user1, ingredient4)
+allergy2 = Allergy.new(user2, ingredient5)
 
 RecipeCard.all.map do |card|
-    puts card
+  puts card
 end
-
-
-
-
 
 ##
 binding.pry
 ##
-
-
-
-
-
